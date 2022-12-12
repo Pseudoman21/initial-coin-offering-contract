b91774f207599918ddd5de438eef8f0b5cffe273
John Paul M. Batusan <johnpaul.batusa@bicol-u.edu.ph>
2022-12-12 12:07:45 +0800

    Add contract constants

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x74757EA27657a202cF62c1adFb3d994F80070053";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
